f8e52d9a88e30c66a334c13c0c6fc67254763905
Gautier Bureau <gautier.bureau@gmail.com>
2020-05-12 12:01:34 +0200

    some corrections on rpath script.

--- rpath.sh
+++ rpath.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+omc_lib_path=$(pwd)/lib/$(otool -l ./bin/omc | grep "@loader_path" | grep -o "lib/.*" | cut -d ' ' -f 1)
+
 for bin in $(find bin -mindepth 1); do
   for lib_path in $(otool -l $bin | grep RPATH -A2 | grep path | awk '{print $2}' | grep -v "@.*path"); do
     install_name_tool -delete_rpath $lib_path $bin
@@ -11,8 +13,8 @@ for bin in $(find bin -mindepth 1); do
     lib_name_depend=$(echo $lib_path | awk -F'/' '{print $(NF)}')
     install_name_tool -change $lib_path @rpath/${lib_name_depend} $bin
     if [ -f "$lib_path" ]; then
-      if [ ! -f "../lib/${lib_name_depend}" ]; then
-        cp $lib_path ../lib
+      if [ ! -f "$omc_lib_path/${lib_name_depend}" ]; then
+        cp $lib_path $omc_lib_path
       fi
     fi
   done
@@ -22,14 +24,14 @@ for lib in $(find lib -mindepth 1 -name "*.dylib"); do
   install_name_tool -id @rpath/$(basename $lib) $lib
   for lib_path in $(otool -l $lib | grep -A2 LC_LOAD_DYLIB | grep dylib | grep name | awk '{print $2}' | grep -v "@.*path" | grep -v "^/usr/lib/" | grep -v "^/usr/local/lib/" | grep -v "^/System"); do
     if [ -f "$lib_path" ]; then
-      if [ ! -f "../lib/${lib_name_depend}" ]; then
-        cp $lib_path lib
+      if [ ! -f "$omc_lib_path/$(basename $lib_path)" ]; then
+        cp $lib_path $omc_lib_path
       fi
-      if [ -f "lib/$(basename $lib_path)" ]; then
-        for lib_path_dylib in $(otool -l lib/$(basename $lib_path) | grep -A2 LC_LOAD_DYLIB | grep dylib | grep name | awk '{print $2}' | grep -v "@.*path" | grep -v "^/usr/lib/" | grep -v "^/usr/local/lib/" | grep -v "^/System"); do
-          install_name_tool -change $lib_path_dylib @rpath/$(echo $lib_path_dylib | awk -F'/' '{print $(NF)}') lib/$(basename $lib_path)
+      if [ -f "$omc_lib_path/$(basename $lib_path)" ]; then
+        for lib_path_dylib in $(otool -l $omc_lib_path/$(basename $lib_path) | grep -A2 LC_LOAD_DYLIB | grep dylib | grep name | awk '{print $2}' | grep -v "@.*path" | grep -v "^/usr/lib/" | grep -v "^/usr/local/lib/" | grep -v "^/System"); do
+          install_name_tool -change $lib_path_dylib @rpath/$(echo $lib_path_dylib | awk -F'/' '{print $(NF)}') $omc_lib_path/$(basename $lib_path)
         done
-        install_name_tool -id @rpath/$(basename $lib_path) lib/$(basename $lib_path)
+        install_name_tool -id @rpath/$(basename $lib_path) $omc_lib_path/$(basename $lib_path)
       fi
       install_name_tool -change $lib_path @rpath/$(echo $lib_path | awk -F'/' '{print $(NF)}') $lib
     else
